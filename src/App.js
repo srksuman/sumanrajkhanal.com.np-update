910c4a57b5825bfb175bf4e5bb3b9c4a95449a9d
suman khanal <sumanrajkhanal@gmail.com>
2021-03-10 23:09:52 +0545

    project setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css" ; 
+function App() {
+  return (
+    <div>
+     <h2>Hello react</h2>
+    </div>
+  );
+}
+
+export default App;
